b9f505b1425f39433c40c3d331ff5c114a50260c
Michael Potter <mgp140@gmail.com>
2023-04-10 10:00:15 -0400

    feat: jspm generated import maps

--- eleventy.config.cjs
+++ eleventy.config.cjs
@@ -1,14 +1,17 @@
 const pluginWebC = require("@11ty/eleventy-plugin-webc");
+const fs = require("fs");
+const path = require("path");
+const importmaps = fs.readFileSync(path.join('./importmap.json'), 'utf8');
 
 module.exports = function(eleventyConfig) {
 	eleventyConfig.addPlugin(pluginWebC, {
 		components: "website/_includes/components/*.webc",
 	});
 
+  eleventyConfig.addGlobalData("importmap", importmaps);
 
 	eleventyConfig.setServerPassthroughCopyBehavior("passthrough");
 	eleventyConfig.addPassthroughCopy({ public: "." });
-
 
   eleventyConfig.setServerOptions({
 		module: "@11ty/eleventy-server-browsersync",
---
+++ entrypoints.js
@@ -0,0 +1,5 @@
+import "@rhds/elements/rh-footer/rh-footer.js";
+import "@rhds/elements/rh-cta/rh-cta.js";
+import "@rhds/elements/rh-navigation-secondary/rh-navigation-secondary.js";
+import "@rhds/elements/rh-tabs/rh-tabs.js";
+import "@rhds/elements/rh-blockquote/rh-blockquote.js";
