ac1575fdca06ddcd2b069847e5e806953d7cabaa
Steven Spriggs <steven.spriggs@gmail.com>
2023-07-10 14:03:38 -0400

    feat: add translations

--- eleventy.config.cjs
+++ eleventy.config.cjs
@@ -1,11 +1,23 @@
 const pluginWebC = require("@11ty/eleventy-plugin-webc");
 const bundlerPlugin = require("@11ty/eleventy-plugin-bundle");
 
+const { EleventyI18nPlugin } = require("@11ty/eleventy");
+
+const i18n = require('eleventy-plugin-i18n');
+const translations = require('./website/_data/i18n');
+
 module.exports = function(eleventyConfig) {
 	eleventyConfig.addPlugin(pluginWebC, {
 		components: "website/_includes/components/*.webc",
 	});
   eleventyConfig.addPlugin(bundlerPlugin);
+
+	eleventyConfig.addPlugin(EleventyI18nPlugin, {
+    // any valid BCP 47-compatible language tag is supported
+    defaultLanguage: "en", // Required, this site uses "en"
+  });
+
+  eleventyConfig.addPlugin(i18n, translations);
 
 	eleventyConfig.setServerPassthroughCopyBehavior("passthrough");
 	eleventyConfig.addPassthroughCopy({ public: "." });
@@ -20,6 +32,23 @@ module.exports = function(eleventyConfig) {
     ui: false,
     ghostMode: false,
   });
+
+  eleventyConfig.setBrowserSyncConfig({
+    callbacks: {
+      ready: function (err, bs) {
+        bs.addMiddleware('*', (req, res) => {
+          if (req.url === '/') {
+            res.writeHead(302, {
+              location: '/en/'
+            });
+            res.end();
+          }
+        });
+      }
+    }
+  });
+
+  
 
 	return {
 		dir: {
