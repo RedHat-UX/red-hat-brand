d1497df04b96d8d18084d69df03fc977101d627c
Steven Spriggs <steven.spriggs@gmail.com>
2023-07-14 15:06:18 -0400

    feat: add 404 page, upgrade RHDS (#65)
    * feat: add 404 page, upgrade RHDS
    
    * fix: add netlify config to redirect to 404 if not found
    
    * fix: add links to 404 page
    
    * fix: add one more link to 404
    
    * fix: lite-youtube-embed removed from importmap by accident
    
    * fix: missing webc:keep on importmap

--- eleventy.config.cjs
+++ eleventy.config.cjs
@@ -1,5 +1,8 @@
+const fs = require("fs");
 const pluginWebC = require("@11ty/eleventy-plugin-webc");
 const bundlerPlugin = require("@11ty/eleventy-plugin-bundle");
+
+const NOT_FOUND_PATH = "_site/404.html";
 
 module.exports = function(eleventyConfig) {
 	eleventyConfig.addPlugin(pluginWebC, {
@@ -19,6 +22,23 @@ module.exports = function(eleventyConfig) {
     notify: false,
     ui: false,
     ghostMode: false,
+    callbacks: {
+      ready: function(err, bs) {
+
+        bs.addMiddleware("*", (req, res) => {
+          if (!fs.existsSync(NOT_FOUND_PATH)) {
+            throw new Error(`Expected a \`${NOT_FOUND_PATH}\` file but could not find one. Did you create a 404.html template?`);
+          }
+
+          const content_404 = fs.readFileSync(NOT_FOUND_PATH);
+          // Add 404 http status code in request header.
+          res.writeHead(404, { "Content-Type": "text/html; charset=UTF-8" });
+          // Provides the 404 content without redirect.
+          res.write(content_404);
+          res.end();
+        });
+      }
+    }
   });
 
 	return {
