03c972aecb2b43d749135e8fa8bfe7ce291fc3fa
Steven Spriggs <steven.spriggs@gmail.com>
2023-07-17 14:30:53 -0400

    feat: add ssr support

--- public/assets/javascript/main.js
+++ public/assets/javascript/main.js
@@ -3,5 +3,6 @@ import "@rhds/elements/rh-navigation-secondary/rh-navigation-secondary.js";
 import "@rhds/elements/rh-footer/rh-footer.js";
 import "@rhds/elements/rh-footer/rh-footer-universal.js";
 import "@rhds/elements/rh-tabs/rh-tabs.js";
+import "@rhds/elements/rh-card/rh-card.js";
 import "@rhds/elements/rh-blockquote/rh-blockquote.js";
 import "lite-youtube-embed";
